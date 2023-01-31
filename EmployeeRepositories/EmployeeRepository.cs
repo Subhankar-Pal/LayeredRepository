f5e7fe63b65b7511778b67600e6bae643ab61d96
Subhankar <Subhankar>
2023-01-31 15:21:42 +0530

    First Time

---
+++ EmployeeRepositories/EmployeeRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EmployeeRepositories.Data;
+using EmployeeRepositories.DTO;
+
+namespace EmployeeRepositories
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        EfCoreDbContext db;
+        public EmployeeRepository() { }
+        public EmployeeRepository(EfCoreDbContext context)
+        {
+            this.db = context;  
+        }
+        public List<EmployeeDTO> GetEmployees()
+        {
+            EmployeeDTO employeeDTO= new EmployeeDTO(); 
+           // employee.Department.ToString();
+            var q = db.Employees.Select(e => new EmployeeDTO
+            {
+                EmployeeName = e.EmployeeName,
+                DepartmentName = e.Department.DepartmentName,
+                EmployeeId = e.EmployeeId,
+                Salary = e.Salary,
+                JoinDate = e.JoinDate
+
+            }).ToList();
+
+            return q;
+        }
+    }
+}
---
+++ LayeredApi/Controllers/EmployeesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using EmployeeRepositories;
+
+namespace LayeredApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private IEmployeeRepository empRepo;
+        public EmployeesController(IEmployeeRepository empRepo)
+        {
+            this.empRepo = empRepo;
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            
+            var empList = empRepo.GetEmployees();
+            return Ok(empList);
+        }
+    }
+}
